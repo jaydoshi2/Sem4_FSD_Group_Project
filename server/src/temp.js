4ffce421892f37950e28f9f467107bb93c0f177b
Jay D Doshi <workingjay2010@gmail.com>
2024-07-22 11:41:48 +0530

    added the changes in the server

--- server/src/temp.js
+++
@@ -1,9 +0,0 @@
-// const { text } = require('express');
-import TranscriptAPI from 'youtube-transcript-api';
-let fullText= ''
-
-
-// for (let index = 0; index < array.length; index++) {
-//    console.log(index)
-    
-// }
