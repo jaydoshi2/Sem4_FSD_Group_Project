bab4a76bda7366d97b8d2ea720d14d84a66da570
Jay D Doshi <workingjay2010@gmail.com>
2024-07-26 11:42:02 +0530

    added slight backend

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+const auth = require('../middleware/auth');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/google', authController.googleAuth);
+router.get('/google/callback', authController.googleCallback);
+router.post('/logout', auth, authController.logout);
+
+module.exports = router;
