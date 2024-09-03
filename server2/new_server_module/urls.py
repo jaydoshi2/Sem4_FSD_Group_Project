c64d0f288d22b8b54e848d917495e42fefa6e286
Jay D Doshi <workingjay2010@gmail.com>
2024-09-03 14:13:50 +0530

    new_server_module has been removed

--- server2/new_server_module/urls.py
+++
@@ -1,9 +0,0 @@
-# new_server_module/urls.py
-from django.urls import path
-from . import views
-from .views import ChatbotAPI
-
-urlpatterns = [
-    path('handle-api-keys/', views.handle_api_keys, name='handle_api_keys'),
-     path('api/chatbot/', ChatbotAPI.as_view(), name='chatbot'),
-]
