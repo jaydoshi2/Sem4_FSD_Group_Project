8eca09161adf3283b1fddcf57a42d66be0082e2d
Jay D Doshi <workingjay2010@gmail.com>
2024-10-05 16:34:49 +0530

    whole client directory updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,53 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom"
-import Login from './pages/Login'
-import Signup from './pages/SignUp';
-import Course from './pages/Course';
+import React, { useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { default as Chatbot, default as ChatbotComponent } from './components/ChatBot';
+import Footer from './components/Footer';
+import NavBar from './components/Navbar';
 import ResetPassword from './components/ResetPassword';
-import ForgotPassword from './pages/ForgotPassword';
+import AboutPage from './pages/AboutPage';
+import ContactSection from './pages/ContactSection';
+import Course from './pages/Course';
 import CourseDetails from './pages/CourseDetails';
 import CourseDisplay from './pages/CourseDisplay';
-import Profile from './pages/Profile';
-import UserCoursesPage from './pages/UserCourse';
-import ChatbotComponent from './components/ChatBot';
-import ContactSection from './pages/ContactSection';
-import VideoPage from './pages/VideoPage';
-import LeaderBoard from './pages/LeaderBoard';
-import Chatbot from './components/ChatBot';
+import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
+import LeaderBoard from './pages/LeaderBoard';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Signup from './pages/SignUp';
 import Skills from './pages/Skills';
-import NavBar from './components/Navbar';
-import Footer from './components/Footer';
-import About from './pages/About';
+import UserCoursesPage from './pages/UserCourse';
+import VideoPage from './pages/VideoPage';
+import Certificate from './pages/Certificate';
+import AdminDashboard from './pages/AdminPage';
 const App = () => {
-  
+
+
   return (
     <div className="App">
       <Chatbot />
-      <NavBar/>
+      <NavBar />
       <Routes>
+        <Route path=" /reset-password/:token" element={<ResetPassword />} />
         <Route path="/contact-us" element={<ContactSection />} />
         <Route path="/chatbot" element={<ChatbotComponent />} />
-        <Route path="/reset-password/:resetToken" element={<ResetPassword />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path="/" element={<Home />} />
         <Route path="/Home1" element={<Course />} />
-        <Route path='/login' element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path='/contact' element={<ContactSection />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/home' element={<Home />} />
         <Route path="/course" element={<CourseDisplay />} />
-        <Route path="/courseDetails" element={<CourseDetails />} />
+        <Route path="/courseDetails/" element={<CourseDetails />} />
         <Route path="/my-courses" element={<UserCoursesPage />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/skill" element={<Skills />} />
         <Route path="/video" element={<VideoPage />} />
-        <Route path='/about' element={<About />} />
-
+        <Route path='/aboutpage' element={<AboutPage />} />
         <Route path="/leaderBoard" element={<LeaderBoard />} />
+        <Route path="/certificate/:courseId" element={<Certificate />} />
+        <Route path="/admin/*" element={<AdminDashboard />} />
       </Routes>
       <Footer />
     </div>
