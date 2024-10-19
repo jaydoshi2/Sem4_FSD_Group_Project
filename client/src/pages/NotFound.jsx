ccca3700bf1035b5ff3398557ff8aa19fcfd5c39
archanpatel1425 <archanpatel6070@gmail.com>
2024-10-19 20:22:04 +0530

    not found page bugs solved

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import styles from '../styles/PageNotFound.module.scss';
+import { useNavigate } from 'react-router-dom';
+import NotFoundIMG from '../assets/NotFound.jpg';
 
-const NotFoundPage = () => {
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
   return (
-    <div className={styles.face}>
-      <div className={styles.band}>
-        <div className={styles.red}></div>
-        <div className={styles.white}></div>
-        <div className={styles.blue}></div>
-      </div>
-      <div className={styles.eyes}></div>
-      <div className={styles.dimples}></div>
-      <div className={styles.mouth}></div>
-
-      <h1 className={styles.heading}>404 PAGE NOT FOUND!</h1>
-      <div className={styles.btn} onClick={() => window.location.href = '/'}>Return to Home</div>
+    <div className="flex flex-col items-center justify-center min-h-screen p-6">
+      <img 
+        src={NotFoundIMG} 
+        alt="Not Found" 
+        className="w-full md:w-2/3 lg:w-1/2 h-auto md:h-2/3 object-cover mb-6" // Adjusted for responsiveness
+      />
+      <button
+        onClick={handleGoHome}
+        className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-800 transition duration-300 focus:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-yellow-300"
+      >
+        Back to Home
+      </button>
     </div>
   );
 };
 
-export default NotFoundPage;
+export default NotFound;
